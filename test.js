e345ba2ae97abf82d0e7f54225849a2c1f9fd4e2
Turing machine <igor@Turings-Air.digi.box>
2022-11-12 03:57:07 +0100

    Refactored the code from previous commit using the new Number class, representing a number on the board

--- test.js
+++ test.js
@@ -1,86 +1,67 @@
 const origin = document.getElementById("origin")
 
-const main = document.querySelector('main')
+const CELL_LENGTH = `106`   // TODO: make this constant adapt to the particular board dimensions
 
-let board = [[-1, -1, -1, -1],
-             [-1, -1, -1, -1],
-             [-1, -1, -1, -1],
-             [-1, -1, -1, -1]]
-
-
-const colors = {
+const COLORS = {
     "1" : "green",
     "2" : "brown",
     "4" : "blue"
-}
+}       
 
-const CELL_LENGTH = `106` 
+// The object COLORS defines the color of a number depending on it's value
 
-// TODO: make this constant adapt to the particular board dimensions
+// The Number class represents numbers on the board
 
-async function spawnNumber (value, x, y) {
-    // insert exception here
-    let newNumber = document.createElement("div")
-    newNumber.setAttribute("class", "number")
-    newNumber.style.backgroundColor = colors[value.toString()]
-    newNumber.addEventListener("click", () => {
+class Number {
+    constructor(cellX, cellY, value) {
+        this.body = document.createElement("div")
+        this.body.style.backgroundColor = this.backgroundColor = COLORS[value.toString()]
+        this.body.setAttribute("class", "number")
+        this.body.style.top = `${cellY * CELL_LENGTH}px`
+        this.body.style.left = `${cellX * CELL_LENGTH}px`
+        this.x = cellX
+        this.y = cellY
+        this.value = value
+        this.valueElement = document.createElement("h1")
+        this.valueElement.innerText = `${value}`
+        this.body.appendChild(this.valueElement)
+        origin.appendChild(this.body)
 
         
 
-
-    })
-    newNumber.style.top = `${y * CELL_LENGTH}px`
-    newNumber.style.left = `${x * CELL_LENGTH}px`
-    const newNumberValue = document.createElement("h1")
-    newNumberValue.innerText = `${value}`
-    newNumber.appendChild(newNumberValue)
-    origin.appendChild(newNumber)
-    board[x][y] = value
-
-    newNumber.addEventListener("click", () => {
-        moveNumLeft(newNumber)
-    })
-    return newNumber
-}
-
-
-async function moveNumRight(num) {
-    const translationTiming = {
-        duration: 500, iterations: 1
+        this.body.addEventListener("click", () => {
+            this.move(1, 0)
+        })
     }
-    const numTranslation = [
-        { left: num.style.left},
-        { left: '318px' }
-      ];
-    await num.animate(numTranslation, translationTiming)
-    num.style.left = `${3 * CELL_LENGTH}px`
-    console.log("click registered")
-}
 
-async function moveNumLeft(num) {
-    const translationTiming = {
-        duration: 500, iterations: 1
+    move = async (destinationX, destinationY) => {
+        const translationTiming = {
+            duration: 500, iterations: 1
+        }
+        const numTranslation = [
+            {
+                top: this.body.style.top,
+                left: this.body.style.left
+            },
+            {
+                top: `${destinationY * CELL_LENGTH}px`,
+                left: `${destinationX * CELL_LENGTH}px`
+            }
+        ]
+        await this.body.animate(numTranslation, translationTiming)
+
+        this.body.style.top = `${destinationY * CELL_LENGTH}px`
+        this.body.style.left = `${destinationX * CELL_LENGTH}px`
     }
-    const numTranslation = [
-        { right: num.style.right},
-        { right: '0px' }
-      ];
-    await num.animate(numTranslation, translationTiming)
-    num.style.right = "0px"
-    console.log("click registered")
 }
 
+// experimental initial setup of the board
 
+const someNum = new Number(3, 2, 1)
 
-// initial settings for now
+const num1 = new Number(1, 3, 2)
 
-spawnNumber(2, 1, 3)
-
-spawnNumber(1, 1, 2)
-
-spawnNumber(4, 3, 1)
-
-
+const num2 = new Number(3, 1, 4)
 
 
 
